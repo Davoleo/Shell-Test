a7f78f711522ebd2bd672b40175bf62e39f2e8ec
Davoleo <davoleo2001@gmail.com>
2019-07-17 19:58:12 +0200

    Arithmetic and Variables

---
+++ main/arithmetic.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# You cannot have space between the equals and the members
+myName="Davoleo"
+
+# Constant variable
+declare -r NUM1=5
+
+num2=4
+
+# Arithmetics
+num3=$((NUM1+num2))
+num4=$((NUM1-num2))
+num5=$((NUM1*num2))
+num6=$((NUM1/num2))
+
+echo "5 + 4 = $num3"
+echo "5 - 4 = $num4"
+echo "5 * 4 = $num5"
+echo "5 / 4 = $num6"
+
+# Power
+echo $((5**2))
+# Modulus - Division Remainder
+echo $(( 5%4 ))
+
+#shorthand operators
+# += -= *= /=
+# They Replace: var = var + 1
+rand=5
+let rand+=4
+echo "$rand"
+echo "rand++ = $(( rand++ ))"
+echo "++rand = $(( ++rand ))"
+echo "rand-- = $(( rand-- ))"
+echo "--rand = $(( --rand ))"
