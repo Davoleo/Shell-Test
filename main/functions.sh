b0e9d6f13346fdd06e52772a95f351632f5be53b
Davoleo <davoleo2001@gmail.com>
2019-08-02 01:46:26 +0200

    Functions

--- main/functions.sh
+++ main/functions.sh
@@ -6,7 +6,35 @@ prints on many
 lines
 END
 
+getDate(){
+    date
+    return
+}
+getDate
 
+#Global Variable (Available EVERYWHERE in this file)
+name="Davoleo"
+
+demLocal(){
+    local name="Dave"
+    return
+}
+demLocal
+echo $name
+
+#Parameters are addressed using $ + the number of the parameter in the order it was given
+getSum(){
+    local num3=$1
+    local num4=$2
+
+    local sum=$((num3+num4))
+    echo $sum
+}
+num1=5
+num2=8
+
+sum=$(getSum num1 num2)
+echo "The sum is: $sum"
 
 #PAUSE function.
 function pause() {
